ff5e2653876d0ae3cddf756749180d15b2489ccc
Fadeoc Khaos <fadeoc@gmail.com>
2016-06-06 13:55:55 +0800

    textareaLimiting finished

---
+++ textareaLimiting.js
@@ -0,0 +1,22 @@
+// limit certain length of the input text in a textarea
+// usage: textareaClipping(textareaId:string[, lengthLimit:int])
+//          textareaId -> textarea id,
+//          lengthLimit -> chars length to limit, omit granted with default value 50000 chars.
+function textareaClipping(textareaId, lengthLimit = 50000)
+{
+    var textarea = document.getElementById(textareaId);
+    if (textarea)
+    {
+        var len = lengthLimit;
+        function limitChars()
+        {
+            if (textarea.value.length > len)
+            {
+                textarea.value = textarea.value.substr(0, len);
+            }
+        }(len)
+        textarea.addEventListener("click", limitChars);
+        textarea.addEventListener("keyup", limitChars);
+        textarea.addEventListener("keydown", limitChars);
+    }
+}
